acdbc85c35cff9e3242bc500d753a9655975c213
Abduaziz3455 <abduaziz3455@gmail.com>
2024-05-06 16:54:09 +0500

    voice api

--- apis/v1/route_chat.py
+++ apis/v1/route_chat.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status
+import requests
+from fastapi import APIRouter, status, UploadFile, File
 from fastapi import Depends
 from sqlalchemy import delete, table
 from sqlalchemy.orm import Session
@@ -21,6 +22,31 @@ async def invoke_agent_with_retry(query: str, user_id):
     return await agent.ainvoke({"input": query}, config={"configurable": {"session_id": user_id}})
 
 
+async def stt(file):
+    url = 'https://mohir.ai/api/v1/stt'
+    headers = {
+        "Authorization": '8d700409-246f-4799-bf6f-3d1cdfea7561:c204f060-db45-46f0-b5ed-d6a5db47c13a'
+    }
+
+    files = {
+        "file": ("audio.mp3", file),
+    }
+    data = {
+        "return_offsets": "true",
+        "run_diarization": "false",
+        "blocking": "true",
+    }
+
+    try:
+        response = requests.post(url, headers=headers, files=files, data=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"Request failed with status code {response.status_code}: {response.text}"
+    except requests.exceptions.Timeout:
+        return "Request timed out. The API response took too long to arrive."
+
+
 @router.get("/")
 async def get_status():
     return {"status": "running"}
@@ -34,6 +60,20 @@ async def send_message(query: QueryInput, db: Session = Depends(get_db)):
     response["intermediate_steps"] = [str(s) for s in response["intermediate_steps"]]
     create_new_message(response.copy(), db)
     return response
+
+
+@router.post("/stt_agent")
+async def upload_voice(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        response = await stt(contents)
+        # with open(file.filename, 'wb') as f:
+        #     f.write(contents)
+        return response
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
 
 
 @router.get("/delete_history", status_code=status.HTTP_200_OK)
