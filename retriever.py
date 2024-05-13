9a552f32253155c69860a084cfc0e73ede27b0e9
Abduaziz3455 <abduaziz3455@gmail.com>
2024-05-13 15:27:23 +0500

    retriever can take strings!

--- retriever.py
+++ retriever.py
@@ -13,7 +13,7 @@ from langchain_core.vectorstores import VectorStore
 room_prompt = PromptTemplate(input_variables=["question"], template="""As an expert in text extraction, your task is to extract room numbers from user questions. 
     
     ### Instructions: Your goal is to identify and extract any room numbers mentioned in the user's question. 
-    If the user's question contains a room number, return the number. For example, if the user's question is '2xonali uylar narxi qancha?' 
+    If the user's question contains a room number, return the number. For example, if the user's question is 'ikki xonali uylar narxi qancha?' 
     you should only return '2' without text. If no room number is detected, return 'None'. 
     
     ### Context: You are developing a text extraction algorithm that specifically focuses on identifying room numbers within user queries. 
