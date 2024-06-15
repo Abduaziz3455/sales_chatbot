2f10c3746d5d805a65fa181c6ce05b12b7dfb528
Abduaziz3455 <abduaziz3455@gmail.com>
2024-06-15 17:48:59 +0500

    extract chain added

--- prompt_maker.py
+++ prompt_maker.py
@@ -2,14 +2,14 @@ from dotenv import load_dotenv
 from langchain import hub
 from langchain.chains.llm import LLMChain
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
+# from langchain_anthropic import ChatAnthropic
 
 load_dotenv()
-chat_model = ChatAnthropic(model='claude-3-haiku-20240307', temperature=0.5)
-# chat_model = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.5)
+# chat_model = ChatAnthropic(model='claude-3-haiku-20240307', temperature=0.5)
+chat_model = ChatOpenAI(model='gpt-4o', temperature=0.7)
 prompt = hub.pull("hardkothari/prompt-maker")
 chain = LLMChain(llm=chat_model, prompt=prompt, verbose=True, output_key='output')
-task = 'Column Variable Replacement'
+task = 'Text extraction'
 prompt = input("Prompt: ")
 response = chain({'task': task, 'lazy_prompt': prompt})
 with open('generated_prompt.txt', 'w', encoding='utf-8') as outfile:
--- retriever.py
+++ retriever.py
@@ -1,8 +1,7 @@
-import re
 from typing import List
 
 import pandas as pd
-from langchain.chains.llm import LLMChain
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 from langchain_core.callbacks import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
 from langchain_core.prompts import PromptTemplate
@@ -13,14 +12,27 @@ from environs import Env
 
 env = Env()
 
-room_prompt = PromptTemplate(input_variables=["question"], template="""As an expert in text extraction, your task is to extract room numbers from user questions. 
-    
-    ### Instructions: Your goal is to identify and extract any room numbers mentioned in the user's question. 
-    If the user's question contains a room number, return the number. For example, if the user's question is 'ikki xonali uylar narxi qancha?' 
-    you should only return '2' without text. If no room number is detected, return 'None'. 
-    
-    ### Context: You are developing a text extraction algorithm that specifically focuses on identifying room numbers within user queries. 
-    Your algorithm needs to accurately recognize and extract room numbers to provide relevant responses. Original question: {question}""")
+
+def extract_chain(text: str):
+    schemas = [
+        ResponseSchema(name="Rooms", description="The number of rooms the client wants. Options: from 1 to 6 or null", type='number'),
+        ResponseSchema(name="Floor",
+                       description="The floor number the client wants an apartment on. Options: from 1 to 20 or null", type='number'),
+        ResponseSchema(name="FlatStatus",
+                       description="The status of the flat, indicating whether the apartment is repaired or not. Options: 1 (repaired), 0 (not repaired), or null", type='number'),
+        ResponseSchema(name="Area",
+                       description="The desired area of the apartment in square meters. Options: from 1 to 200 or null", type='number')]
+
+    output_parser = StructuredOutputParser.from_response_schemas(schemas)
+    format_instructions = output_parser.get_format_instructions()
+    prompt = PromptTemplate(template="As an expert in text extraction, please carefully analyze the given client question about apartment features. "
+                                     "If there is nothing to extract, respond with `null`.{format_instructions}\n{text}.",
+                            input_variables=["text"],
+                            partial_variables={"format_instructions": format_instructions})
+    chat_model = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
+    chain = prompt | chat_model | output_parser
+    dict_response = chain.invoke({"text": text})
+    return dict_response
 
 
 class FlatRetriever(BaseRetriever):
@@ -28,7 +40,8 @@ class FlatRetriever(BaseRetriever):
     """Number of top results to return"""
     db: VectorStore
 
-    def _get_relevant_documents(self, dict_query: dict, *, run_manager: CallbackManagerForRetrieverRun) -> List[Document]:
+    def _get_relevant_documents(self, dict_query: dict, *, run_manager: CallbackManagerForRetrieverRun) -> List[
+        Document]:
         """Sync implementations for retriever."""
         query = dict_query["question"]
         documents = self.db.as_retriever(search_kwargs={"k": 50}).get_relevant_documents(query)
@@ -39,12 +52,10 @@ class FlatRetriever(BaseRetriever):
             dataframes.append(df.loc[document.metadata['row'], :])
         df = pd.DataFrame(dataframes)
         # room extractor
-        chat_model = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
-        chain = LLMChain(llm=chat_model, prompt=room_prompt, verbose=False, output_key='rooms')
-        text = chain({'question': query})['rooms']
-        if text != 'None':
+        response = extract_chain(query)
+        room = response['Rooms']
+        if room != 'None':
             try:
-                room = int(re.findall(r'\d+', text)[0])
                 df = df[df['rooms'] == room].sort_values(by=['total_price_sum'], ascending=True)
             except:
                 pass
